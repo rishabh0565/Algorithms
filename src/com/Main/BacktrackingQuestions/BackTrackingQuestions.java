4756bb95d10a626e01b495116f548b74c9ec4a62
ASUS <ASUS@DESKTOP-EA0HTEP>
2024-03-14 01:05:48 +0530

    Added King And Knight patterns too

--- src/com/Main/BacktrackingQuestions/BackTrackingQuestions.java
+++ src/com/Main/BacktrackingQuestions/BackTrackingQuestions.java
@@ -6,11 +6,172 @@ public class BackTrackingQuestions {
 		int n = 4;
 		boolean [][] board = new boolean[n][n];
 //		display(board);
-System.out.println(queenPlacement(board, 0));
+//System.out.println(queenPlacement(board, 0));
+kingPlacement(board, 0, 0,4);
 	}
 	
+	public static void  kingPlacement(boolean [][] board, int rows, int cols,int max) {
+	    if(max == 0) {
+	    	displayKingBoard(board);
+	    	System.out.println();
+	    	return;
+	    }	    
+	    if(cols == board[0].length-1 && rows == board.length-1) {
+	    	return;    		
+	    }  
+	    if(cols == board[0].length) {	        
+	    	kingPlacement(board, rows+1, 0 , max);
+		    return;
+		    }
+	    if( isSafeToPlaceKing(board, rows, cols)) {
+	    	board[rows][cols] = true;
+	    	kingPlacement(board, rows, cols+1,max-1);
+	    	board[rows][cols] = false;
+	    }
+	    kingPlacement(board, rows, cols+1 , max);
+ 
+	}
+	
+	private static boolean isSafeToPlaceKing(boolean[][] board, int rows, int cols) {
+	     if(isValidForKing(board, rows, cols-1)) {
+	    	 if(board[rows][cols-1]) {
+	    		 return false;
+	    	 }
+	     }
+	     if(isValidForKing(board, rows, cols+1)) {
+	    	 if(board[rows][cols+1]) {
+	    		 return false;
+	    	 }
+	     }
+	     if(isValidForKing(board, rows-1, cols)) {
+	    	 if(board[rows-1][cols]) {
+	    		 return false;
+	    	 }
+	     }
+	     
+	     if(isValidForKing(board, rows-1, cols+1)) {
+	    	 if(board[rows-1][cols+1]) {
+	    		 return false;
+	    	 }
+	     }
+	     if(isValidForKing(board, rows-1, cols-1)) {
+	    	 if(board[rows-1][cols-1]) {
+	    		 return false;
+	    	 }
+	     }
+		return true;
+	}
+
+
+
+
+	private static boolean isValidForKing(boolean[][] board, int rows, int cols) {
+		if(rows<board.length && rows>=0&& cols<board[0].length &&cols>=0) {
+			return true;
+		}
+	
+		return false;
+	}
+
+
+
+
+	private static void displayKingBoard(boolean[][] board) {
+		   for(boolean [] arrr : board) {
+			   for(int i = 0 ; i < arrr.length ; i++) {
+				   if(arrr[i] == false) {
+					   System.out.print(". ");
+				   }
+				   else {
+					   System.out.print("K ");
+				   }
+			   }
+			   System.out.println();
+		   }
+		}
 	
 	
+	
+	
+	/////////////////////////////////////////////////////
+	public static void  knightPlacement(boolean [][] board, int rows, int cols,int max) {
+	    if(max == 0) {
+	    	displayKnightBoard(board);
+	    	System.out.println();
+	    	return;
+	    }	    
+	    if(cols == board[0].length-1 && rows == board.length-1) {
+	    	return;    		
+	    }  
+	    if(cols == board[0].length) {	        
+		    knightPlacement(board, rows+1, 0 , max);
+		    return;
+		    }
+	    if( isSafeToPlaceKnight(board, rows, cols)) {
+	    	board[rows][cols] = true;
+	    	knightPlacement(board, rows, cols+1,max-1);
+	    	board[rows][cols] = false;
+	    }
+      knightPlacement(board, rows, cols+1 , max);
+ 
+	}
+	
+	private static boolean isSafeToPlaceKnight(boolean[][] board, int rows, int cols) {
+	     if(isValid(board, rows-1, cols-2)) {
+	    	 if(board[rows-1][cols-2]) {
+	    		 return false;
+	    	 }
+	     }
+	     if(isValid(board, rows-1, cols+2)) {
+	    	 if(board[rows-1][cols+2]) {
+	    		 return false;
+	    	 }
+	     }
+	     
+	     if(isValid(board, rows-2, cols-1)) {
+	    	 if(board[rows-2][cols-1]) {
+	    		 return false;
+	    	 }
+	     }
+	     if(isValid(board, rows-2, cols+1)) {
+	    	 if(board[rows-2][cols+1]) {
+	    		 return false;
+	    	 }
+	     }
+		return true;
+	}
+
+
+
+
+	private static boolean isValid(boolean[][] board, int rows, int cols) {
+		if(rows<board.length && rows>=0&& cols<board[0].length &&cols>=0) {
+			return true;
+		}
+	
+		return false;
+	}
+
+
+
+
+	private static void displayKnightBoard(boolean[][] board) {
+		   for(boolean [] arrr : board) {
+			   for(int i = 0 ; i < arrr.length ; i++) {
+				   if(arrr[i] == false) {
+					   System.out.print(". ");
+				   }
+				   else {
+					   System.out.print("K ");
+				   }
+			   }
+			   System.out.println();
+		   }
+		}
+	
+	
+	
+/////////////////////////////////////////////////////////////////////////	
 	
 	public static int queenPlacement(boolean[][] board, int rows) {
 		if(rows == board.length) {
